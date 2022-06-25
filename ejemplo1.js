6c690f4414d1534604e51875bda92a990824b70e
Hamiltonpiamba <amiltonchi@gmail.com>
2022-06-24 20:33:10 -0500

    segundo comit

---
+++ ejemplo1.js
@@ -0,0 +1,22 @@
+class Libros{
+ constructor(autor,nombre,numeropaginas){
+this.autor=autor
+this.nombre=nombre
+this.numeropaginas=numeropaginas
+ }
+set  nombrelibro(nombre1){
+
+}
+get mostrarlibro(){
+
+}
+
+}
+
+let libro1= new Libros("Gabriel garcia marquez","cien años de soledad","350")
+console.log(libro1)
+let libro2= new Libros("julio verne","El centro de la tierra","200")
+console.log(libro2)
+let libro3= new Libros("cristiano ronaldo","soy leyenda","100")
+console.log(libro3)
+
